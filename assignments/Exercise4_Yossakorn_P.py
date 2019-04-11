2c5c0f2a99060d0a506e688705a7ce37acfba9e2
jameyossakorn <00teef@gmail.com>
2019-04-11 15:34:17 +0700

    Create Exercise4_Yossakorn_P.py

---
+++ assignments/Exercise4_Yossakorn_P.py
@@ -0,0 +1,9 @@
+FoundationEnglish = 50.0
+GeneralBusiness = 74.2
+ICS = 40.0
+ComputerPrograming = 80.0
+print("---Your Score---")
+print("Foundation English = ",FoundationEnglish)
+print("General Business = ",GeneralBusiness)
+print("Introduction to Computer Systems = ",ICS)
+print("Computer Programming = ",ComputerPrograming)
